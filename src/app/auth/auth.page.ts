7913d871f2fa6ea984d1db464b4e79161e0cea47
RitaJain <prayasjain1996@gmail.com>
2020-05-14 01:54:04 +0530

    added auth form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -12,6 +13,7 @@ export class AuthPage implements OnInit {
   constructor(private authService : AuthService, private router: Router,
     private loadingCtrl: LoadingController) { }
 
+    isLoginMode: boolean = true;
   ngOnInit() {
   }
 
@@ -24,9 +26,24 @@ export class AuthPage implements OnInit {
       loadingEl.dismiss();
       this.router.navigateByUrl('/places/tabs/discover');
     }, 1500);
-    })
-    
-    
+    });
   }
 
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+    if (this.isLoginMode) {
+      // send req to login server
+    } else {
+      // send req to signup server
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLoginMode = !this.isLoginMode; 
+  }
 }
